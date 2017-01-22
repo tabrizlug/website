5ac0d756e54903a3b854207b758ce7ae69d835da
royaflash <royaflash@yahoo.com>
2017-01-22 11:40:12 +0330

    445 session

---
+++ contents/entries/445.php
@@ -0,0 +1,30 @@
+<div class="type">
+	Session
+</div>
+
+<div class="title">
+آشنایی با git - گیت
+</div>
+
+<div class="subject">
+آشنایی با گیت - git
+</div>
+
+<div class="date">
+1395/11/05
+</div>
+
+<div class="body">
+<center>   . با حول و قوت الهی جلسه 445 را برگزار خواهیم کرد .</center>
+
+ 
+این جلسه پیرامون گیت - git   توسط :‌ یاشار اسمعیل دخت  ارایه خواهد شد 
+<center><img src="./445.jpg" width=600 height=300> </center>
+ 
+ <div id="evand"><div id="evand-content"></div><div class="evand-copyright"><a href="http://evand.com" title="جلسه ۴۴۴ با ارایه یاشار اسمعیل دخت"><img src="" alt="جلسه ۴۴۵ با ارایه یاشار اسمعیل دخت"></a></div></div><script type="text/javascript">var evand=evand||{};evand.theme="default";evand.eventSlug="tabrizlug445",function(){var t=document.createElement("script");t.type="text/javascript",t.async=!0,t.src=("https:"==document.location.protocol?"https://":"http://")+"static.evand.com/embed/script.js?load=1";var e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(t,e)}();</script>
+<br />
+<br />
+</div>
+
+
+
